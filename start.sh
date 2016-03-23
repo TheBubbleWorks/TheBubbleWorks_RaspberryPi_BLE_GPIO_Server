49ff93f18640eb6955b6d0fa9881d4677f46b4ab
WayneKeenan <wayne.keenan@gmail.com>
2016-03-23 09:43:38 +0000

    added start script

---
+++ start.sh
@@ -0,0 +1,11 @@
+echo --- Turning off ALL Bluetooth adatpers.
+echo
+hciconfig | grep hci| cut -d ':' -f 1 | xargs -I % sudo hciconfig %I down
+echo --- Turing on 'USB bus type' Bluetooth adatpers only.
+echo
+hciconfig | grep USB | cut -d ':' -f 1 | xargs -I % sudo hciconfig %I up
+echo --- Adadtper status:
+echo
+hciconfig
+node main.js
+
