d3cf82d3b6c5ca7d3077ac6333b4e93c353696f4
WayneKeenan <wayne.keenan@gmail.com>
2016-03-23 16:54:55 +0000

    start script can accept HCI bus type, added for testing bleno

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+
+BUS_TYPE=${1:-USB}
+
 echo --- Stopping bluetooth
 echo
 sudo systemctl stop bluetooth
@@ -6,9 +11,9 @@ echo --- Turning off ALL Bluetooth adatpers.
 echo
 hciconfig | grep hci| cut -d ':' -f 1 | xargs -I % sudo hciconfig %I down
 
-echo --- Turing on 'USB bus type' Bluetooth adatpers only.
+echo --- Turing on $BUS_TYPE bus type Bluetooth adatpers only.
 echo
-hciconfig | grep USB | cut -d ':' -f 1 | xargs -I % sudo hciconfig %I up
+hciconfig | grep $BUS_TYPE | cut -d ':' -f 1 | xargs -I % sudo hciconfig %I up
 
 echo --- Adadtper status:
 echo
