7b9a985ad55c06a23d8904fa51ffed3cf149a3c7
WayneKeenan <wayne.keenan@gmail.com>
2016-03-24 07:21:37 +0000

    adding pwm support

--- test/test_led.py
+++ test/test_led.py
@@ -2,13 +2,14 @@ import os
 import RPi.GPIO as GPIO
 import time
 
+LED_PIN = 23
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(27, GPIO.OUT)
+GPIO.setup(LED_PIN, GPIO.OUT)
 
 for x in range(1,5):
-    GPIO.output(27,1)
+    GPIO.output(LED_PIN,1)
     time.sleep(0.5)
-    GPIO.output(27,0)
+    GPIO.output(LED_PIN,0)
     time.sleep(0.5)
 
 GPIO.cleanup()
