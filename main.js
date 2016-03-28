38a859163dc09bb3271fcf03769c359e1129b6db
WayneKeenan <wayne.keenan@gmail.com>
2016-03-28 19:00:08 +0000

    added test ytonh SSL server and web bluetooth test page. added eddystone beacon

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-process.env['BLENO_DEVICE_NAME'] = 'BubblePi';
+DEVICE_NAME = 'CamJamEduKit3';
+
+
+
+process.env['BLENO_DEVICE_NAME'] = DEVICE_NAME;
 
 // Note that you may need to require a nested version of bleno
 // See https://github.com/don/node-eddystone-beacon/issues/30
@@ -8,8 +12,8 @@ var bleno = require('bleno');
 //var bleno = require('eddystone-beacon/node_modules/bleno');
 var eddystoneBeacon = require('eddystone-beacon');
 
-//var WebSocket = require('ws');
-//var ws = new WebSocket('ws://localhost:8000');
+var WebSocket = require('ws');
+var ws = new WebSocket('ws://localhost:8000');
 
 var UARTService = require('./services/uart/uart-service');
 var uartService = new UARTService(onUARTReceiveData);
@@ -39,7 +43,113 @@ function onUARTReceiveData(data) {
     return true;
 }
 
+function handleError(error) {
+    console.log("ERROR: " + error);
+}
 
+
+
+bleno.on('stateChange', function(state) {
+    console.log('on -> stateChange: ' + state);
+
+    if (state === 'poweredOn') {
+        start_advertising_flipflop();
+    } else {
+
+    }
+});
+
+
+
+
+var BEACON_ADV_STATE= 0;
+var GATT_ADV_STATE = 1
+
+
+var FLIPFLOP_TIME = 2000;
+
+var flipFlopIntervalTimer;
+var flipFlopEnabled = true;;
+var advertisingState = 0;
+
+function start_advertising_flipflop() {
+
+    flipFlopIntervalTimer = setInterval(function () {
+
+        if (!flipFlopEnabled)
+            return;
+
+        try {
+            advertisingState = 1 - advertisingState;
+
+
+            if (advertisingState == BEACON_ADV_STATE) {
+                console.log("FLIFLOP: BEACON_ADV_STATE");
+
+                stop_service_advertising();
+                start_beacon_advertising();
+            } else {
+                console.log("FLIFLOP: GATT_ADV_STATE");
+                stop_beacon_advertising();
+                start_service_advertising();
+            }
+
+        } catch(err)
+        {
+            console.log("ERROR: " + JSON.stringify(err));
+        }
+    }, FLIPFLOP_TIME);
+
+}
+
+function stop_advertising_flipflop() {
+    if (flipFlopIntervalTimer) {
+        clearInterval(flipFlopIntervalTimer);
+        flipFlopIntervalTimer = undefined;
+    }
+}
+
+
+function start_beacon_advertising() {
+    console.log("start_beacon_advertising");
+    var url = 'https://192.168.1.73:9443'
+    eddystoneBeacon.advertiseUrl(url, { name: DEVICE_NAME }, { txPowerLevel: -25 });  //
+}
+
+function stop_beacon_advertising() {
+    console.log("stop_beacon_advertising");
+    eddystoneBeacon.stop();
+    bleno.stopAdvertising();
+    bleno.disconnect();
+}
+
+// -- Non Eddystone beacon bleno code
+function start_service_advertising() {
+    console.log("start_service_advertising");
+    bleno.startAdvertising(DEVICE_NAME, [uartService.uuid]);
+}
+
+function stop_service_advertising() {
+    console.log("stop_service_advertising");
+    bleno.stopAdvertising();
+    bleno.disconnect();
+}
+
+bleno.on('advertisingStart', function(error) {
+    console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+
+    if (!error) {
+        //if (advertisingState = GATT_ADV_STATE) {
+            console.log("Advertising Services");
+            bleno.setServices([
+                uartService
+            ]);
+        //}
+    }
+});
+
+
+/*
 bleno.once('advertisingStart', function(err) {
   if (err) {
     throw err;
@@ -48,46 +158,22 @@ bleno.once('advertisingStart', function(err) {
   console.log('on -> advertisingStart');
   bleno.setServices([
       uartService
-  ]);
+  ], handleError);
 });
-
-eddystoneBeacon.advertiseUrl('https://goo.gl/BlLKHs');
-
+*/
 
 
 
-// -- Non Eddystone beacon bleno code
-
-
-/*
-bleno.on('stateChange', function(state) {
-    console.log('on -> stateChange: ' + state);
-
-    if (state === 'poweredOn') {
-        bleno.startAdvertising('BubbleWorks', [uartService.uuid]);
-    } else {
-        bleno.stopAdvertising();
-    }
-});
-
-bleno.on('advertisingStart', function(error) {
-    console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
-
-    if (!error) {
-        bleno.setServices([
-            //gpioService,
-            uartService
-        ]);
-    }
-});
 
 
 bleno.on('accept', function(clientAddress) {
     console.log('on -> stateAccept: ' + clientAddress);
+    flipFlopEnabled = false;
 });
 
 bleno.on('disconnect', function(clientAddress) {
     console.log('on -> disconnect: ' + clientAddress);
+    flipFlopEnabled = true;
 });
 
 bleno.on('rssiUpdate', function(rssi) {
@@ -111,6 +197,38 @@ bleno.on('advertisingStart', function(error) {
 bleno.on('advertisingStartError', function(error) {
     console.log('on -> advertisingStartError, error: ' + error);
 });
+
+bleno.on('advertisingStop', function(error) {
+    console.log('on -> advertisingStop, error?: ' + error);
+
+});
+
+
+
+
+/*
+
+ bleno.on('stateChange', function(state) {
+        console.log('on -> stateChange: ' + state);
+
+        if (state === 'poweredOn') {
+            bleno.startAdvertising('BubbleWorks', [uartService.uuid]);
+        } else {
+            bleno.stopAdvertising();
+        }
+    });
+
+    bleno.on('advertisingStart', function(error) {
+        console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+
+        if (!error) {
+            bleno.setServices([
+                //gpioService,
+                uartService
+            ]);
+        }
+    });
+
 
 */
 
